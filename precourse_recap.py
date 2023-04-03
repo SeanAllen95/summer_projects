841ab6c157622b5afd569779d402211201e81b3a
Sean <scallen11@btinternet.com>
2023-04-03 20:12:38 +0100

    my first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+# Homework week_01 Day_01
+
+current_week = 1
+current_day= 1
+total_weeks =  16
+total_days = 5
+
+total_course_days = total_weeks * total_days
+
+course_days_left = total_course_days - current_day
+
+def course_length():
+    print(total_course_days)
+
+
+def days_left():
+    print(course_days_left)
+
+course_length()
+days_left()
